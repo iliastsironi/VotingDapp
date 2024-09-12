cf10eda21a3cdd23ca7622f9b5414bd03ad20130
iliastsironi <hliastsironis@gmail.com>
2024-09-12 16:18:56 +0300

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config(); // Load environment variables
 
 module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/4764281883ed494d8485a6af3b400616", // Replace with your Infura or Alchemy project URL
-      accounts: [`0xdde2d3159171430db1f9139b5906391322197429dea5b24572c7b568be527bdf`] // Replace with your MetaMask private key
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`, // Use INFURA_PROJECT_ID from .env
+      accounts: [`0x${process.env.PRIVATE_KEY}`] // Use PRIVATE_KEY from .env
     }
   }
 };
